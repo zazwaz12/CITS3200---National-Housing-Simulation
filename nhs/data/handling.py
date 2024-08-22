76a2a832dd142793ddc8846bf9c1b12766e6cfad
SodaVolcano <tin1771718335@gmail.com>
2024-08-22 19:37:03 +0800

    extended to csv, xlsx, renamed to read_spreadsheets

--- nhs/data/handling.py
+++ nhs/data/handling.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Optional
+from typing import Callable, Literal
 import os
 import polars as pl
 from nhs.utils.path import list_files
@@ -7,26 +7,61 @@ from loguru import logger
 
 
 @logger.catch()
-def read_psv(file_path: str) -> Optional[pl.LazyFrame]:
+def read_psv(file_path: str) -> pl.LazyFrame | None:
     """
     Load a .psv file into a polars LazyFrame, returning None if exception occurs
     """
     return pl.scan_csv(file_path, separator="|")
 
 
-def read_all_psv(file_dir_pattern: str) -> dict[str, pl.LazyFrame | None]:
+@logger.catch()
+def read_csv(file_path: str) -> pl.LazyFrame | None:
     """
-    Return dictionary of key and polars LazyFrames given directory of .psv files
+    Load a .csv file into a polars LazyFrame, returning None if exception occurs
+    """
+    return pl.scan_csv(file_path)
+
+
+@logger.catch()
+def read_xlsx(file_path: str) -> pl.LazyFrame | None:
+    """
+    Load a .xlsx file into a polars `LazyFrame`, returning None if exception occurs
+
+    **NOTE**: Polars use `xlsx2csv` to read .xlsx files, so whole CSV file is read
+    """
+    return pl.read_excel(file_path).lazy()
+
+
+def __get_spreadsheet_reader(
+    file_extension: str,
+) -> Callable[[str], pl.LazyFrame | None]:
+    return {
+        ".psv": read_psv,
+        ".csv": read_csv,
+        ".xlsx": read_xlsx,
+    }[file_extension]
+
+
+def read_spreadsheets(
+    file_dir_pattern: str, extension: Literal["csv", "psv", "xlsx"]
+) -> dict[str, pl.LazyFrame | None]:
+    """
+    Return dictionary of key and polars `LazyFrame` given directory of PSV, CSV, or XLSX files.
 
     If a file cannot be read, the value will be None.
+
+    **Warning**: If file is XLSX, the whole file is read (i.e. no lazy evaluation). A `LazyFrame` is
+    still returned for consistency.
 
     Parameters
     ----------
     file_dir_pattern: str
-        Path to directory containing .psv files. Optionally, you can include the name of
+        Path to directory containing .psv, .csv, or .xlsx files. Optionally, you can include the name of
         the PSV file with a placeholder `"{key}"` - the resulting dictionary will use
         the string specified by `"{key}"` as the key. If omitted, the file name will be
         used as the key. See example.
+    extension: str
+        File extension to read. Must be one of `psv`, `csv`, or `xlsx`.
 
     Returns
     -------
@@ -39,20 +74,23 @@ def read_all_psv(file_dir_pattern: str) -> dict[str, pl.LazyFrame | None]:
         - file1.psv
         - file2.psv
 
-    >>> read_all_psv("path/to/psv_files/")
+    >>> read_spreadsheets("path/to/psv_files/")
     {'file1.psv': <LazyFrame>, 'file2.psv': <LazyFrame>}
-    >>> read_all_psv("path/to/psv_files/{key}.psv")
+    >>> read_spreadsheets("path/to/psv_files/{key}.psv")
     {'file1': <LazyFrame>, 'file2': <LazyFrame>}
-    >>> read_all_psv("path/to/psv_files/file{key}.psv")
+    >>> read_spreadsheets("path/to/psv_files/file{key}.psv")
     {'1': <LazyFrame>, '2': <LazyFrame>}
     """
-    psv_files = list_files(os.path.dirname(file_dir_pattern))
-    psv_files = list(filter(lambda x: x.endswith(".psv"), psv_files))
+    files = list_files(os.path.dirname(file_dir_pattern))
+    files = list(filter(lambda x: x.endswith(f".{extension}"), files))
+    reader = __get_spreadsheet_reader(f".{extension}")
+
     if "{key}" not in file_dir_pattern:
-        keys = map(os.path.basename, psv_files)
+        keys = map(os.path.basename, files)
     else:
         # placeholder_matches return list of tuples ("key",), [0] to get "key"
         keys = map(
-            lambda x: x[0], placeholder_matches(psv_files, file_dir_pattern, ["key"])
+            lambda x: x[0], placeholder_matches(files, file_dir_pattern, ["key"])
         )
-    return {key: val for key, val in zip(keys, map(read_psv, psv_files))}
+
+    return {key: val for key, val in zip(keys, map(reader, files))}
--- tests/test_data/test_handling.py
+++ tests/test_data/test_handling.py
@@ -1,12 +1,15 @@
 from gzip import READ
 from pytest_mock import MockerFixture
 import polars as pl
+
 from ..context import nhs
 
-read_all_psv = nhs.data.handling.read_all_psv
+read_spreadsheets = nhs.data.handling.read_spreadsheets
 
 LIST_FILES_PATCH = "nhs.data.handling.list_files"
 READ_PSV_PATCH = "nhs.data.handling.read_psv"
+READ_CSV_PATCH = "nhs.data.handling.read_csv"
+READ_XLSX_PATCH = "nhs.data.handling.read_xlsx"
 
 
 class TestReadAllPsv:
@@ -19,7 +22,7 @@ class TestReadAllPsv:
         )
         mocker.patch(READ_PSV_PATCH, side_effect=[pl.LazyFrame(), pl.LazyFrame()])
 
-        result = read_all_psv("path/to/psv_files/")
+        result = read_spreadsheets("path/to/psv_files/", "psv")
 
         assert len(result) == 2
         assert "file1.psv" in result
@@ -27,12 +30,30 @@ class TestReadAllPsv:
         assert isinstance(result["file1.psv"], pl.LazyFrame)
         assert isinstance(result["file2.psv"], pl.LazyFrame)
 
+    def test_reads_all_xlsx_files(self, mocker: MockerFixture):
+        mocker.patch(
+            LIST_FILES_PATCH,
+            return_value=[
+                "path/to/xlsx_files/file1.xlsx",
+                "path/to/xlsx_files/file2.xlsx",
+            ],
+        )
+        mocker.patch(READ_XLSX_PATCH, side_effect=[pl.LazyFrame(), pl.LazyFrame()])
+
+        result = read_spreadsheets("path/to/xlsx_files/", "xlsx")
+
+        assert len(result) == 2
+        assert "file1.xlsx" in result
+        assert "file2.xlsx" in result
+        assert isinstance(result["file1.xlsx"], pl.LazyFrame)
+        assert isinstance(result["file2.xlsx"], pl.LazyFrame)
+
     # Directory contains no .psv files
     def test_no_psv_files_in_directory(self, mocker: MockerFixture):
         mock_list_files = mocker.patch(LIST_FILES_PATCH)
         mock_list_files.return_value = []
 
-        result = read_all_psv("path/to/psv_files/")
+        result = read_spreadsheets("path/to/psv_files/", "psv")
 
         assert result == {}
 
@@ -40,11 +61,11 @@ class TestReadAllPsv:
     def test_read_all_psv_with_placeholder(self, mocker: MockerFixture):
         mocker.patch(
             LIST_FILES_PATCH,
-            return_value=["path/to/psv_files/file1.psv", "path/to/psv_files/file2.psv"],
+            return_value=["path/to/csv_files/file1.csv", "path/to/csv_files/file2.csv"],
         )
-        mocker.patch(READ_PSV_PATCH, side_effect=[pl.LazyFrame(), pl.LazyFrame()])
+        mocker.patch(READ_CSV_PATCH, side_effect=[pl.LazyFrame(), pl.LazyFrame()])
 
-        result = read_all_psv("path/to/psv_files/fil{key}.psv")
+        result = read_spreadsheets("path/to/csv_files/fil{key}.csv", "csv")
 
         assert len(result) == 2
         assert "e1" in result
