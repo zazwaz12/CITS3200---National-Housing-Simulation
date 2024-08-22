b9586224c5df75c6a37b0efe2229576e4d4f1f09
LeyiChen62 <167012851+LeyiChen62@users.noreply.github.com>
2024-08-23 00:11:25 +0800

    Update and rename filter_sa1_regions to filter_sa1_regions.py
    Added function filter_SA1_region_codes for filtering SA1 regions

---
+++ nhs/filter/filter_sa1_regions.py
@@ -0,0 +1,26 @@
+from typing import List
+import polars as pl
+
+
+def filter_SA1_region_codes(lf: pl.LazyFrame, SA1_column: str = "SA1_CODE_2021", region_codes: List[str] = []) -> pl.LazyFrame:
+    """
+    Filters the LazyFrame to include only rows with specified SA1 area codes.
+
+    Parameters
+    ----------
+    lf : pl.LazyFrame
+        The LazyFrame containing the census data to be filtered.
+    SA1_column : str
+        The name of the column containing the SA1 area codes. Defaults to "SA1_CODE_2021".
+    region_codes : List[str]
+        A list of SA1 area codes to filter for.
+
+    Returns
+    -------
+    pl.LazyFrame
+        A LazyFrame containing only rows with the specified SA1 area codes.
+    """
+    if not region_codes:
+        return lf  # If no region codes are provided for filtering, return the original LazyFrame
+    
+    return lf.filter(pl.col(SA1_column).is_in(region_codes))
