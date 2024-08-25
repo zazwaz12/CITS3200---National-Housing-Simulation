d1fe1da1e14e0ba9cab3f057e8c41fe7515c50ac
Reece030130 <167398987+Reece030130@users.noreply.github.com>
2024-08-25 18:58:16 +0800

    Update column_fix.py

--- nhs/data/column_fix.py
+++ nhs/data/column_fix.py
@@ -56,12 +56,3 @@ def column_readable(prev_csv_pl_df: dict[str, LazyFrame | None], xlsx_pl_df) -> 
         except KeyError:
             continue
     return prev_csv_pl_df
-
-
-# if __name__ == '__main__':
-#     new_pl_df = column_readable(read_spreadsheets(
-#         'C:/Users/IamNo/Desktop/DataFiles/FilesIn/census/2021 Census GCP All Geographies for AUS/AUS/'
-#         '2021Census_G01_AUS_AUS.csv', 'csv'), read_spreadsheets('C:/Users/IamNo/Desktop/DataFiles/FilesIn/census'
-#                                                                 '/Metadata/Metadata_2021_GCP_DataPack_R1_R2.xlsx',
-#                                                                 'xlsx'))
-#     print(new_pl_df["2021Census_G02_AUS_AUS.csv"].collect().collect_schema().names())
