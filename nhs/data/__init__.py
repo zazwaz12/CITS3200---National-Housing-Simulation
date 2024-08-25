3faae088b76a3a4e5c1bffa88172fe710cb24329
Reece0301 <22857062@student.uwa.edu.au>
2024-08-25 18:46:47 +0800

    __init__.py

--- nhs/data/__init__.py
+++ nhs/data/__init__.py
@@ -1,6 +1,6 @@
 from .handling import read_spreadsheets, read_psv, read_csv, read_xlsx
 from .column_fix import column_readable
 
-__all__ = ["read_spreadsheets", "read_psv", "read_xlsx", "read_csv", "column_readable"]
+__all__ = ["read_spreadsheets", "read_psv", "read_csv", "read_xlsx", "column_readable"]
 
 
