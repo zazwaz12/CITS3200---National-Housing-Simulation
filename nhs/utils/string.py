7687329bd7d098987752e8569b53bfbf5b458d6e
SodaVolcano <tin1771718335@gmail.com>
2024-08-26 18:00:11 +0800

    oh my fuck I forgot the code again

--- nhs/utils/string.py
+++ nhs/utils/string.py
@@ -49,21 +49,6 @@ def capture_placeholders(
     x = x.replace("\x01", re_pattern)
     return str(x)
 
-    return tz.pipe(
-        [s] + placeholders,
-        # Replace placeholders to avoid them being escaped
-        curried.reduce(
-            lambda string, placeholder: string.replace(f"{{{placeholder}}}", "\x00")
-        ),
-        # Replace all non-capturing placeholders with a different symbol
-        lambda string: re.sub(r"{[a-zA-Z0-9_]*}", "\x01", string),
-        re.escape,
-        # Encase provided placeholders in parentheses to create capturing groups
-        lambda string: string.replace("\x00", f"({re_pattern})"),
-        lambda string: string.replace("\x01", re_pattern),
-        str,
-    )
-
 
 @log_entry_exit()
 def placeholder_matches(
