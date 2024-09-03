c070250edb9c705cc410957d01965445024fcf4b
SodaVolcano <tin1771718335@gmail.com>
2024-09-04 00:08:45 +0800

    logger catch

--- scripts/spreadsheet_to_parquet.py
+++ scripts/spreadsheet_to_parquet.py
@@ -14,25 +14,32 @@ list_files = nhs.utils.path.list_files
 get_reader = nhs.data.handling.get_spreadsheet_reader
 
 
+@logger.catch()
+def save_parquet(path: str, input_dir: str, output_dir: str):
+    relative_path = Path(path).relative_to(input_dir)
+    output_file_path = Path(output_dir) / relative_path.with_suffix(".parquet")
+
+    if output_file_path.exists():
+        logger.warning(f"File {output_file_path} already exists. Skipping.")
+        return
+
+    output_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+    df = get_reader(Path(path).suffix)(path)
+    if not isinstance(df, pl.LazyFrame):
+        logger.error(f"Failed to read {path}")
+        return
+    df.collect().write_parquet(output_file_path)
+
+
 def convert_to_parquet(input_dir: str, output_dir: str):
     paths = list_files(input_dir)
-    paths = filter(lambda x: x.endswith((".xlsx", ".xls", ".csv", ".psv")), paths)
+    paths = list(filter(lambda x: x.endswith((".xlsx", ".xls", ".csv", ".psv")), paths))
 
-    for path in tqdm(paths, desc="Converting spreadsheets to Parquet"):
-        relative_path = Path(path).relative_to(input_dir)
-        output_file_path = Path(output_dir) / relative_path.with_suffix(".parquet")
-
-        if output_file_path.exists():
-            logger.warning(f"File {output_file_path} already exists. Skipping.")
-            continue
-
-        output_file_path.parent.mkdir(parents=True, exist_ok=True)
-
-        df = get_reader(Path(path).suffix)(path)
-        if not isinstance(df, pl.LazyFrame):
-            logger.error(f"Failed to read {path}")
-            continue
-        df.collect().write_parquet(output_file_path)
+    for path in tqdm(
+        paths, desc="Converting spreadsheets to Parquet", total=len(paths)
+    ):
+        save_parquet(path, input_dir, output_dir)
 
 
 def main():
