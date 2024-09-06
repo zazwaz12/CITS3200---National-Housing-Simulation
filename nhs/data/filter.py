240c2c2d095de82857f4c30d288813c246eee3bc
LeyiChen62 <23442577@student.uwa.edu.au>
2024-09-06 17:47:32 +0800

    Refactor filter_sa1_regions to make region_codes a required parameter

--- nhs/data/filter.py
+++ nhs/data/filter.py
@@ -2,7 +2,7 @@ from typing import List
 import polars as pl
 
 
-def filter_sa1_regions(lf: pl.LazyFrame, sa1_column: str = "SA1_CODE_2021", region_codes: List[str] = []) -> pl.LazyFrame:
+def filter_sa1_regions(lf: pl.LazyFrame, region_codes: List[str], sa1_column: str = "SA1_CODE_2021") -> pl.LazyFrame:
     """
     Filters the LazyFrame to include only rows with specified SA1 area codes.
 
@@ -10,18 +10,17 @@ def filter_sa1_regions(lf: pl.LazyFrame, sa1_column: str = "SA1_CODE_2021", regi
     ----------
     lf : pl.LazyFrame
         The LazyFrame containing SA1 region codes and data to be filtered.
-    SA1_column : str
-        The name of the column containing the SA1 area codes. Defaults to "SA1_CODE_2021".
     region_codes : List[str]
         A list of SA1 area codes to filter for.
+    SA1_column : str
+        The name of the column containing the SA1 area codes. Defaults to "SA1_CODE_2021".
 
     Returns
     -------
     pl.LazyFrame
         A LazyFrame containing only rows with the specified SA1 area codes.
     """
-    if not region_codes:
-        return lf  # If no region codes are provided, return the original LazyFrame
-    
     return lf.filter(pl.col(sa1_column).is_in(region_codes))
+
+
 
