463aebce18c19ddaafd91a7d60e462e44004ba0d
LeyiChen62 <23442577@student.uwa.edu.au>
2024-09-06 16:19:38 +0800

    Save current changes in issue-29

--- nhs/data/__init__.py
+++ nhs/data/__init__.py
@@ -1,4 +1,4 @@
 from .handling import read_csv, read_psv, read_spreadsheets, read_xlsx
 from .filter import filter_sa1_regions
 
-__all__ = ["read_spreadsheets", "read_csv", "read_psv", "read_xlsx"]
+__all__ = ["read_spreadsheets", "read_csv", "read_psv", "read_xlsx", "filter_sa1_regions"]
