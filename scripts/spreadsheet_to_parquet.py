9c134134f4e80ce4399bc19dd31d550fe4f99e76
GitHub Action <action@github.com>
2024-09-11 09:17:46 +0000

    Apply automatic formatting

--- scripts/spreadsheet_to_parquet.py
+++ scripts/spreadsheet_to_parquet.py
@@ -1,20 +1,23 @@
 """
 Produce a directory of parquet files from a directory of spreadsheets
 """
+
 import argparse
+import os
 from pathlib import Path
+
 import polars as pl
 from context import nhs
 from loguru import logger
 from tqdm import tqdm
-import os
 
 # Utility functions for file listing and reading spreadsheets
 list_files = nhs.utils.path.list_files
 get_reader = nhs.data.handling.get_spreadsheet_reader
 
+
 @logger.catch()
-def save_parquet(path: str, input_dir: str, output_dir:str):
+def save_parquet(path: str, input_dir: str, output_dir: str):
     # Define relative output file path
     relative_path = Path(path).relative_to(input_dir)
     output_dir = input_dir.replace("FilesIn", "AppStaging")
@@ -24,10 +27,10 @@ def save_parquet(path: str, input_dir: str, output_dir:str):
     if output_file_path.exists():
         logger.warning(f"File {output_file_path} already exists. Skipping.")
         return
-    
+
     if "Standard" in str(path) and "DEFAULT_GEOCODE" not in str(path):
-            logger.warning(f"Only taking DEFAULT GEOCODE from GNAF for now. Skipping.")
-            return
+        logger.warning(f"Only taking DEFAULT GEOCODE from GNAF for now. Skipping.")
+        return
 
     # Create output directories if not exist
     output_file_path.parent.mkdir(parents=True, exist_ok=True)
@@ -39,6 +42,7 @@ def save_parquet(path: str, input_dir: str, output_dir:str):
         return
     df.collect().write_parquet(output_file_path)
 
+
 def convert_to_parquet(input: str, output: str):
     # Filter for supported spreadsheet files
     paths = list_files(input)
@@ -48,18 +52,27 @@ def convert_to_parquet(input: str, output: str):
     for path in tqdm(paths, desc="Converting spreadsheets to Parquet"):
         save_parquet(path, input, output)
 
+
 def main():
     # Setup argument parsing for input and output directories
     parser = argparse.ArgumentParser(
         description="Convert spreadsheets to Parquet files."
     )
 
-    parser.add_argument("input", default= os.path.normpath("./FilesIn"),type=str, help="Input Directory")
-    parser.add_argument("output", default= os.path.normpath("./Appstaging"),type=str, help="Output Directory")
+    parser.add_argument(
+        "input", default=os.path.normpath("./FilesIn"), type=str, help="Input Directory"
+    )
+    parser.add_argument(
+        "output",
+        default=os.path.normpath("./Appstaging"),
+        type=str,
+        help="Output Directory",
+    )
 
     # Execute conversion
     args = parser.parse_args()
     convert_to_parquet(args.input, args.output)
+
 
 if __name__ == "__main__":
     main()
