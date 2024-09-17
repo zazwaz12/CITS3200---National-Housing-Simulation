20f17ad0b701bedbe6e880bcd08d86796c87431a
Reece0301 <22857062@student.uwa.edu.au>
2024-09-17 16:22:04 +0800

    add new query marital_status_gender_comparison_operator_age.

--- nhs/data/category_system.py
+++ nhs/data/category_system.py
@@ -3,6 +3,7 @@ import re
 import polars as pl
 
 from nhs.utils.logging import log_entry_exit
+from nhs.data.handling import specify_row_to_be_header, read_xlsx
 
 
 @log_entry_exit(level="INFO")
@@ -38,18 +39,30 @@ def category_system(category_required: str, data_pack_file: pl.LazyFrame):
     [30-40]
     """
     categories_list = category_required.split("_")
-    result = []
 
-    if (
-        len(categories_list) == 3
-        and re.search(r"\bmale\b|\bfemale\b", categories_list[0], re.IGNORECASE)
-        and re.search(
-            r"\b(over|more than|greater than|above|under|less than|below)\b",
-            categories_list[1],
-            re.IGNORECASE,
+    gender_pattern = r"\bmale\b|\bfemale\b|\bperson\b"
+    age_op_pattern = r"\b(over|more than|greater than|above|under|less than|below)\b"
+    age_value_pattern = r"\b\d+(\.\d+)?\b"
+    marital_status_pattern = r"\bmarried\b|\bwidowed\b"
+
+    def is_valid_category_list(categories):
+        return (
+                len(categories) == 3
+                and re.search(gender_pattern, categories[0], re.IGNORECASE)
+                and re.search(age_op_pattern, categories[1], re.IGNORECASE)
+                and re.search(age_value_pattern, categories[2])
         )
-        and re.search(r"\b\d+(\.\d+)?\b", categories_list[2])
-    ):
+
+    def is_valid_marital_category_list(categories):
+        return (
+                len(categories) == 4
+                and re.search(marital_status_pattern, categories[0], re.IGNORECASE)
+                and re.search(gender_pattern, categories[1], re.IGNORECASE)
+                and re.search(age_op_pattern, categories[2], re.IGNORECASE)
+                and re.search(age_value_pattern, categories[3])
+        )
+
+    if is_valid_category_list(categories_list):
         result = (
             category_for_age_and_gender(
                 data_pack_file,
@@ -60,18 +73,33 @@ def category_system(category_required: str, data_pack_file: pl.LazyFrame):
             .collect()["Long"]
             .to_list()
         )
+    elif is_valid_marital_category_list(categories_list):
+        result = (
+            category_for_age_and_gender(
+                data_pack_file,
+                categories_list[1],
+                categories_list[2].lower(),
+                int(categories_list[3]),
+                categories_list[0].lower()
+            )
+            .collect()["Long"]
+            .to_list()
+        )
+    else:
+        result = []
 
     return result
 
 
 def category_for_age_and_gender(
-    data_pack_file: pl.LazyFrame, gender: str, comparison_operator: str, age: int
-):
+        data_pack_file: pl.LazyFrame, gender: str, comparison_operator: str, age: int, marital_status: str = None
+) -> pl.LazyFrame:
     """
     Filters a `LazyFrame` based on gender and age criteria.
 
     Parameters
     ----------
+    marital_status
     data_pack_file : pl.LazyFrame
         A Polars LazyFrame containing the data to be filtered. It should have at least the columns:
         - age ranges or values
@@ -115,16 +143,17 @@ def category_for_age_and_gender(
     │ Age 30-40  │ Male data                         │
     └────────────┴───────────────────────────────────┘
     """
-    comparison_mapping = {  # type: ignore
-        "over": lambda expr, value: expr > value,  # type: ignore
-        "more than": lambda expr, value: expr > value,  # type: ignore
-        "greater than": lambda expr, value: expr > value,  # type: ignore
-        "above": lambda expr, value: expr > value,  # type: ignore
-        "under": lambda expr, value: expr < value,  # type: ignore
-        "less than": lambda expr, value: expr < value,  # type: ignore
-        "below": lambda expr, value: expr < value,  # type: ignore
+    comparison_mapping = {
+        "over": lambda expr, value: expr > value,
+        "more than": lambda expr, value: expr > value,
+        "greater than": lambda expr, value: expr > value,
+        "above": lambda expr, value: expr > value,
+        "under": lambda expr, value: expr < value,
+        "less than": lambda expr, value: expr < value,
+        "below": lambda expr, value: expr < value,
     }
 
+    # Extract numbers and prepare the filtered LazyFrame
     filtered_lf = (
         data_pack_file.with_columns(
             [
@@ -139,24 +168,41 @@ def category_for_age_and_gender(
             ]
         )
         .filter(
-            (
-                pl.col("Columnheadingdescriptioninprofile")
-                .str.to_lowercase()
-                .str.contains(f"\\b{gender}")
-            )
-            & (pl.col("Long").str.contains("Age"))
+            pl.col("Columnheadingdescriptioninprofile")
+            .str.to_lowercase()
+            .str.contains(f"\\b{gender}")
         )
         .filter(
-            comparison_mapping[comparison_operator](pl.col("first_num"), age)  # type: ignore
+            comparison_mapping[comparison_operator](pl.col("first_num"), age)
             & comparison_mapping[comparison_operator](pl.col("second_num"), age)
-        )
-        .select(
-            [
-                col
-                for col in data_pack_file.collect_schema().names()
-                if col not in ["first_num", "second_num"]
-            ]
         )
     )
 
+    if marital_status:
+        filtered_lf = filtered_lf.filter(
+            pl.col("Columnheadingdescriptioninprofile")
+            .str.to_lowercase()
+            .str.contains(f"\\b{marital_status}")
+        )
+    else:
+        filtered_lf = filtered_lf.filter(
+            pl.col("Long").str.contains("Age")
+        )
+
+    filtered_lf = filtered_lf.select(
+        [
+            col
+            for col in data_pack_file.collect_schema().names()
+            if col not in ["first_num", "second_num"]
+        ]
+    )
+
     return filtered_lf
+
+
+if __name__ == "__main__":
+    path_to_xlsx_file = 'C:/Users/IamNo/Desktop/DataFiles/FilesIn/census/Metadata/Metadata_2021_GCP_DataPack_R1_R2.xlsx'
+    df = read_xlsx(path_to_xlsx_file, 0)
+    new_df = df['Cell Descriptors Information']
+    better_df = specify_row_to_be_header(2, new_df)
+    print(category_system("widowed_female_over_30", better_df))
